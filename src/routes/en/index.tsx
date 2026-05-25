e9b52a419cf6a9f72902123cc71b66cd8c793687
Rochdi (Intralys) <intralys.dev@gmail.com>
2026-05-25 22:23:14 +0100

    feat(routes): Home full composition Navbar + Hero + TrustBand + VSL + Programmes + Testimonials + FinalCTA + Footer (FR + EN)

--- src/routes/en/index.tsx
+++ src/routes/en/index.tsx
@@ -1,15 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { FooterRich } from '@/components/layout/FooterRich';
+import { Navbar } from '@/components/layout/Navbar';
+import { FinalCTASection } from '@/components/sections/FinalCTASection';
 import { Hero } from '@/components/sections/Hero';
+import { ProgramsGrid } from '@/components/sections/ProgramsGrid';
+import { TestimonialGrid } from '@/components/sections/TestimonialGrid';
 import { TrustBand } from '@/components/sections/TrustBand';
 import { VslPlaceholderSection } from '@/components/sections/VslPlaceholderSection';
 
 function HomeEN() {
   return (
-    <main>
-      <Hero />
-      <TrustBand />
-      <VslPlaceholderSection id="methodologie" />
-    </main>
+    <>
+      <Navbar />
+      <main className="pt-[80px]">
+        <Hero />
+        <TrustBand />
+        <VslPlaceholderSection id="methodologie" />
+        <ProgramsGrid />
+        <TestimonialGrid />
+        <FinalCTASection />
+      </main>
+      <FooterRich />
+    </>
   );
 }
 
